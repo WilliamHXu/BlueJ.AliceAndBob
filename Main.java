34e2b898ea9a234669252183663b0b6f2d9f64cb
WilliamXu <williamxu@zipcoders-MacBook-Pro-90.local>
2019-02-05 17:40:32 -0500

    Completed lab

--- Main.java
+++ Main.java
@@ -1,9 +1,20 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.*;  
+
 public class Main {
-
+    
     public static void main(String[] args ){
-
+        Scanner scanner = new Scanner(System.in); 
+        System.out.println("Please enter your name: ");
+        String iname = scanner.nextLine();
+        if (iname.equals("Alice") || iname.equals("Bob")) {
+            System.out.print("Hello " + iname + "!");
     }
+        if (iname.equals("Will")) {
+            System.out.print("All hail the creator!");
+    }
+}
 }
